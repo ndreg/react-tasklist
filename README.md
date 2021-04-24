ceac4cd1f50a2c8f212f842d7386714743405638
Andres <fullbringdres@gmail.com>
2021-04-24 15:37:08 -0500

    02. Se agregan estilos visuales, EDgrid, UXEDteam.

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Foreact | Using useState hook.
 
+A simple and beautiful task list created with React and EDgrid.
+
 **Used tools**
 
 - Vscode
 - Git
+- EDgrid
+- UX EDteam
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,9 +1,25 @@
 import {useState} from 'react'
+import './tasklist.css';
 
 export const TaskList = () => {
+
   return (
-    <div>
+    <>
+      <h1 className="s-center">React Task List</h1>
+      <p className="s-right">foreact</p>
+      <hr/>
+      <form className="ed-container task-form">
+      <input 
+        type="text"
+        name="task"
+        placeholder="Tasks"
+        className="ed-item s-mb-2"
+        ></input>
+        <button className="button dark-color ed-item">Add</button>
+      </form>
+      <ul className="feature-list">
       
-    </div>
+      </ul>
+    </>
   )
 }
